163866b9af78abb0e8eb5b9bd5e2ba59b14acba6
AbsentSemicolon <1569396+AbsentSemicolon@users.noreply.github.com>
2024-08-16 12:09:26 -0500

    Need to be able to open up browser when starting dev

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     base: "",
+    server: {
+        open: true
+    },
     test: {
         globals: true,
         environment: "jsdom",
