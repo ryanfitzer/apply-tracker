5cbed3c1d39a6c485a3af24f5a4384a416b374ab
AbsentSemicolon <1569396+AbsentSemicolon@users.noreply.github.com>
2024-08-28 16:56:48 -0500

    Got a hook test to run
    Needed to modify the test utile and still need some typing to do.

--- src/hooks/chart-hooks.spec.ts
+++ src/hooks/chart-hooks.spec.ts
@@ -1,22 +1,70 @@
-import { JobStatusType } from "../lib/types";
-import { act } from "react";
-import { applicationsActions } from "../store/applications-slice";
-import { renderHook } from "@testing-library/react";
-import useCharts from "./chart-hooks";
-const spy = vi.spyOn(applicationsActions, "replaceApplications");
+import { JobSalaryType, JobStatusType } from "../lib/types";
 
-describe.skip("chart-hook", () => {
+import { act } from "react";
+import { renderWithProviders } from "../utils/test-utils";
+import useCharts from "./chart-hooks";
+
+describe("chart-hook", () => {
     it("something", async () => {
-        spy.mockReturnValue({
-            //@ts-expect-error: stuff
-            "123": {
-                jobStatus: JobStatusType.APPLIED
+        const blah = {
+            preloadedState: {
+                appList: {
+                    items: {
+                        "123": {
+                            jobTitle: "first job",
+                            jobCompany: "company",
+                            jobApplyDate: "2024-07-17",
+                            jobCompanyLink: "https://company.com",
+                            jobLink: "https://company.com/jobs/123",
+                            jobSalary: "",
+                            jobSalaryMax: 0,
+                            jobSalaryMin: 0,
+                            jobSalaryType: JobSalaryType.YR,
+                            jobStatus: JobStatusType.APPLIED,
+                            jobId: "123"
+                        }
+                    }
+                }
             }
-        });
+        };
         let render;
         await act(() => {
-            render = renderHook(() => useCharts());
+            render = renderWithProviders(() => useCharts(), blah, true);
         });
-        expect(render.result.current).toEqual(["1234", false]);
+        expect(render.result.current).toEqual([
+            {
+                meta: {
+                    total: 1,
+                    companyList: [
+                        {
+                            name: "company",
+                            link: "https://company.com"
+                        }
+                    ]
+                },
+                pie: [
+                    ["Phase", "Number"],
+                    ["Applied (1)", 1],
+                    ["Denied (0)", 0],
+                    ["On Hold (0)", 0],
+                    ["Interviewed Scheldued (0)", 0],
+                    ["Interviewed (0)", 0]
+                ],
+                calendar: [
+                    [
+                        {
+                            id: "Date",
+                            type: "date"
+                        },
+                        {
+                            id: "Applications Sent",
+                            type: "number"
+                        }
+                    ],
+                    [new Date("2024-07-17T05:00:00.000Z"), 1]
+                ]
+            },
+            false
+        ]);
     });
 });
--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,11 +1,11 @@
 import type { AppStore, RootState } from '../store';
 import React, { PropsWithChildren } from 'react';
+import { render, renderHook } from '@testing-library/react';
 
 import { I18nextProvider } from 'react-i18next';
 import { Provider } from 'react-redux';
 import type { RenderOptions } from '@testing-library/react';
 import i18n from '../i18n';
-import { render } from '@testing-library/react';
 import setupStore from '../store';
 
 // This type interface extends the default options for render from RTL, as well
@@ -16,16 +16,20 @@ interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
 }
 
 export function renderWithProviders(
-    ui: React.ReactElement,
+    ui: React.ReactElement | any,
     {
         preloadedState = {},
         // Automatically create a store instance if no store was passed in
         store = setupStore(preloadedState),
         ...renderOptions
-    }: ExtendedRenderOptions = {}
+    }: ExtendedRenderOptions = {},
+    isHook: boolean
 ) {
     function Wrapper({ children }: PropsWithChildren<any>): JSX.Element {
         return <I18nextProvider i18n={i18n}><Provider store={store}>{children}</Provider></I18nextProvider>;
+    }
+    if (isHook) {
+        return { store, ...renderHook(ui as any, { wrapper: Wrapper, ...renderOptions }) };
     }
     return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
 }
