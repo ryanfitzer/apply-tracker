d2da31f935842bba70239f486d1c519c776b0c72
AbsentSemicolon <1569396+AbsentSemicolon@users.noreply.github.com>
2024-09-04 12:25:42 -0500

    Only need to show one year right now
    Can make a year selector if there are multiple years to process.

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -13,13 +13,13 @@ const Charts = () => {
     const calendarOptions: GoogleChartOptions = {
         title: "Applications Sent",
         backgroundColor: "",
-        width: 1000
+        width: 1000,
     };
     return (
         <div className="flex flex-col h-full overflow-x-hidden">
             {!loadingChartData && (
                 <div className="flex flex-col">
-                    <div className="flex h-[260px]">
+                    <div className="flex h-[260px] mb-6">
                         <div className="flex-shrink-0">
                             <p>Total Applications: {chartData.meta.total}</p>
                             <p>Company List</p>
@@ -37,7 +37,7 @@ const Charts = () => {
                             />
                         </div>
                     </div>
-                    <div className="overflow-x-auto overflow-y-hidden">
+                    <div className="overflow-y-auto overflow-x-hidden h-full">
                         <div className="">
                             <Chart
                                 chartType="Calendar"
