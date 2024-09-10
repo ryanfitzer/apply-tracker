194d92177f42978a993b3af8475f925e69650903
Christopher Tetreault <1569396+AbsentSemicolon@users.noreply.github.com>
2024-09-10 17:24:14 -0500

    Responsive menu (#15)
    * Created Header Component
    
    * Didn't need changeEvent in app
    
    * Making a menu for responsive sizes
    
    * could not load in demo mode
    
    * Show menu needed for tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 
-import { ChangeEvent, useMemo } from "react";
 import { JobType, SortDirection, UiState } from "./lib/types";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { applicationsActions, selectApplicationEditing, selectApplicationItems, selectApplicationListIsChanged, selectApplicationListViewAs, selectApplicationSort } from "./store/applications-slice";
@@ -15,31 +14,40 @@ import AddJob from "./components/AddJob";
 import Charts from "./components/Charts";
 import DialogModal from "./components/DialogModal";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Job from "./components/Job";
 import JobsTable from "./components/JobTable";
 import { uiActions } from "./store/ui-slice";
 import { useEffect } from "react";
+import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 
 const App = () => {
-    const { i18n, t } = useTranslation();
+    const { t } = useTranslation();
     const dispatch = useAppDispatch();
     const applicationListIsChanged = useAppSelector(selectApplicationListIsChanged);
-    const applicationListSort = useAppSelector(selectApplicationSort);
     const applicationListEditing = useAppSelector(selectApplicationEditing);
+    const applicationListSort = useAppSelector(selectApplicationSort);
     const applicationListItems = useAppSelector(selectApplicationItems);
     const applicationListViewAs = useAppSelector(selectApplicationListViewAs);
     const uiItem = useSelector((state: UiState) => state.ui);
 
     useMemo(async () => {
         const query = new URLSearchParams(document.location.search);
-        const things = await dispatch(fetchBootStrapData());
 
-        if (things) {
-            dispatch(fetchApplicationData(!!query.get("demo")));
+        const isDemo = !!query.get("demo");
+        if (isDemo) {
+            dispatch(fetchApplicationData(true));
         } else {
-            alert('GistID and/or Access Token are not set.');
+            {
+                const bootStrapped = await dispatch(fetchBootStrapData());
+                if (bootStrapped) {
+                    dispatch(fetchApplicationData(true));
+                } else {
+                    alert('GistID and/or Access Token are not set.');
+                }
+            }
         }
     }, [dispatch]);
 
@@ -88,148 +96,53 @@ const App = () => {
         }
     };
 
-    const clearAllJobs = () => {
-        const confirm = window.confirm("are you sure?");
-
-        if (confirm) {
-            dispatch(applicationsActions.removeAllItems());
-        }
-    };
-
-    const changeSortData = (event: ChangeEvent<HTMLSelectElement>) => {
-        const {
-            target: { id, value }
-        } = event;
-
-        dispatch(
-            applicationsActions.sortItemList({
-                by: id === "sortBy" ? value : applicationListSort.by,
-                dir: id === "sortDir" ? value : applicationListSort.dir
-            })
-        );
-    };
-
-    const onChangeLang = (event) => {
-        const lang_code = event.target.value;
-        i18n.changeLanguage(lang_code);
-    };
-
     return (
         <div className="flex flex-col h-full">
-            <header className="flex gap-3 px-4 py-2 justify-between h-10">
-                <div className="flex">
-                    <button
-                        onClick={() => dispatch(uiActions.toggleModal(true))}
-                    >
-                        {t("addJob")}
-                    </button>
-                    <div className="ml-2 border-l-2 pl-2">
-                        <form>
-                            <label>
-                                <span>Sort By: </span>
-                                <select
-                                    className="cursor-pointer"
-                                    name="sortBy"
-                                    id="sortBy"
-                                    onChange={changeSortData}
-                                    value={applicationListSort.by}
+            <div className="flex flex-row h-full overflow-hidden sm:flex-col">
+                <Header />
+                <main className="h-full overflow-y-auto w-full">
+                    {/* Need to compare against 0 to make sure 0 does now show up
+                    * on the UI.
+                    */}
+                    {Object.values(applicationListItems).length > 0 && (
+                        <>
+                            {applicationListViewAs === "table" ? (
+                                <div>
+                                    <JobsTable
+                                        jobs={sortItems(applicationListItems)}
+                                        removeJob={removeJob}
+                                    />
+                                </div>
+                            ) : (
+                                <ResponsiveMasonry
+                                    columnsCountBreakPoints={{
+                                        350: 1,
+                                        750: 2,
+                                        1150: 3,
+                                        1640: 4
+                                    }}
                                 >
-                                    <option value="jobTitle">Title</option>
-                                    <option value="jobApplyDate">Date</option>
-                                </select>
-                                <select
-                                    className="cursor-pointer"
-                                    name="sortDir"
-                                    id="sortDir"
-                                    onChange={changeSortData}
-                                    value={applicationListSort.dir}
-                                >
-                                    <option value={SortDirection.ASCENDING}>Ascending</option>
-                                    <option value={SortDirection.DESCENDING}>Descending</option>
-                                </select>
-                            </label>
-                        </form>
-                    </div>
-                    {/* Disabling for now */}
-                    {/* <div className="ml-2 border-l-2 pl-2">
-                        View As:
-                        <select
-                            className="cursor-pointer"
-                            value={applicationListViewAs}
-                            onChange={(event) =>
-                                dispatch(
-                                    applicationsActions.setViewAs(
-                                        event.target.value
-                                    )
-                                )
-                            }
-                        >
-                            <option value="tiles">Tiles</option>
-                            <option value="table">Table</option>
-                        </select>
-                    </div> */}
-                    <div className="ml-2 border-l-2 pl-2">
-                        <span>Language: </span>
-                        <label htmlFor="langEn" className="cursor-pointer">
-                            <input className="cursor-pointer" id="langEn" type="radio" name="lang" value="en" onClick={onChangeLang}
-                                defaultChecked={i18n.language === "en"} /><span className="pl-1">EN</span>
-                        </label>
-                        <label htmlFor="langEl" className="ml-4 cursor-pointer">
-                            {/* Using Greek language code for now till able to
-                            make a new code. */}
-                            <input className="cursor-pointer" id="langEl" type="radio" name="lang" value="el" onClick={onChangeLang} defaultChecked={i18n.language === "el"} /><span className="pl-1">Satire</span>
-                        </label>
-                    </div>
-                    <div className="ml-2 border-l-2 pl-2">
-                        <button onClick={
-                            () => dispatch(uiActions.toggleChartsModal(true))}>View Stats</button>
-                    </div>
-                </div>
-
-                <button onClick={clearAllJobs}>Clear All Jobs</button>
-            </header>
-            <main className="h-full overflow-y-auto">
-                {/* Need to compare against 0 to make sure 0 does now show up
-                  * on the UI.
-                */}
-                {Object.values(applicationListItems).length > 0 && (
-                    <>
-                        {applicationListViewAs === "table" ? (
-                            <div>
-                                <JobsTable
-                                    jobs={sortItems(applicationListItems)}
-                                    removeJob={removeJob}
-                                />
-                            </div>
-                        ) : (
-                            <ResponsiveMasonry
-                                columnsCountBreakPoints={{
-                                    350: 1,
-                                    750: 2,
-                                    1150: 3,
-                                    1640: 4
-                                }}
-                            >
-                                <Masonry>
-                                    {sortItems(applicationListItems).map(
-                                        (job) => (
-                                            <div
-                                                className="p-3"
-                                                key={job.jobId}
-                                            >
-                                                <Job
-                                                    job={job}
-                                                    removeJob={removeJob}
-                                                />
-                                            </div>
-                                        )
-                                    )}
-                                </Masonry>
-                            </ResponsiveMasonry>
-                        )}
-                    </>
-                )}
-            </main>
+                                    <Masonry>
+                                        {sortItems(applicationListItems).map(
+                                            (job) => (
+                                                <div
+                                                    className="p-3"
+                                                    key={job.jobId}
+                                                >
+                                                    <Job
+                                                        job={job}
+                                                        removeJob={removeJob}
+                                                    />
+                                                </div>
+                                            )
+                                        )}
+                                    </Masonry>
+                                </ResponsiveMasonry>
+                            )}
+                        </>
+                    )}
+                </main>
+            </div>
             <Footer />
             <DialogModal
                 isOpened={uiItem.modalIsVisible}
